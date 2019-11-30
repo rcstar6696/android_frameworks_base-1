43e2dd6212b2f8945e8558475b466d2947efe728
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-11-30 13:41:05 +0100

    SystemUI: Bring back good ol' circle battery style
    * This code is based on the old Java implementation and converted to Kotlin
    
    * Instead of creating different style within the same drawable
      create one on its own, this makes the code more readable and future proof
    
    Change-Id: I23798426e603b277408fb63f4898b385027e7eeb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4785,6 +4785,32 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+        * Display style of the status bar battery information
+        * 0: Display the battery an icon in portrait mode
+        * 1: Display the battery as a circle
+        * 2: Display the battery as plain text
+        * default: 0
+        */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /** @hide */
+        public static final Validator STATUS_BAR_BATTERY_STYLE_VALIDATOR =
+               new SettingsValidators.InclusiveIntegerRangeValidator(0, 2);
+
+        /**
+        * Status bar battery %
+        * 0: Hide the battery percentage
+        * 1: Display the battery percentage inside the icon
+        * 2: Display the battery percentage next to the icon
+        */
+        public static final String STATUS_BAR_SHOW_BATTERY_PERCENT =
+             "status_bar_show_battery_percent";
+
+               /** @hide */
+        public static final Validator STATUS_BAR_SHOW_BATTERY_PERCENT_VALIDATOR =
+             new SettingsValidators.InclusiveIntegerRangeValidator(0, 2);
+
+        /**
         * Swap volume buttons on rotation
         * @hide
         */
@@ -5034,6 +5060,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(LOCKSCREEN_MEDIA_BLUR);
             PRIVATE_SETTINGS.add(ACCELEROMETER_ROTATION_ANGLES);
             PRIVATE_SETTINGS.add(LOCKSCREEN_MEDIA_METADATA);
+            PRIVATE_SETTINGS.add(STATUS_BAR_BATTERY_STYLE);
+            PRIVATE_SETTINGS.add(STATUS_BAR_SHOW_BATTERY_PERCENT);
         }
 
         /**
@@ -5139,6 +5167,8 @@ public final class Settings {
             VALIDATORS.put(LOCKSCREEN_MEDIA_BLUR, LOCKSCREEN_MEDIA_BLUR_VALIDATOR);
             VALIDATORS.put(ACCELEROMETER_ROTATION_ANGLES, ACCELEROMETER_ROTATION_ANGLES_VALIDATOR);
             VALIDATORS.put(LOCKSCREEN_MEDIA_METADATA, LOCKSCREEN_MEDIA_METADATA_VALIDATOR);
+            VALIDATORS.put(STATUS_BAR_BATTERY_STYLE, STATUS_BAR_BATTERY_STYLE_VALIDATOR);
+            VALIDATORS.put(STATUS_BAR_SHOW_BATTERY_PERCENT, STATUS_BAR_SHOW_BATTERY_PERCENT_VALIDATOR);
         }
 
         /**
